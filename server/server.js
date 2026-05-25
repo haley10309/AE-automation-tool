be9cc3c4c5a1bd7863d8c0879854455973d24eee
hayung <yoohayung10309@gmail.com>
2026-05-25 16:12:59 +0900

    fix : server to bcryptjs use

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
